fd99c1cbc94edf1d7fc18927e75be634573c9d71
vo huu nhan <vohuunhan1310@gmail.com>
2024-04-19 15:23:36 +0700

    fix: update logo header

--- components/component/Header.js
+++ components/component/Header.js
@@ -13,13 +13,13 @@ export default function Header(){
                 <div className="container max-w-screen-3xl mx-auto flex flex-row justify-center items-center md:px-2 px-4">
                     <div className="flex flex-row py-5 items-center">
                         <a href="/">
-                            <img src="https://tjbs.ttuscience.org/public/journals/2/pageHeaderLogoImage_en_US.png" className="md:w-[120px] w-[80px]"/>
+                            <img src="/images/logo_ttu.png" className="md:w-[120px] md:ml-2 w-[80px]"/>
                         </a>
-                        <div className="ml-[100px] hidden md:block text-[#183762] text-center text-xl font-semibold">
+                        <div className="md:ml-[60px] ml-[100px] hidden md:block text-[#183762] text-center text-xl font-semibold">
                             <div className={`${raleway.className} drop-shadow-md`}>PHÒNG QUẢN LÝ KHOA HỌC VÀ HỢP TÁC QUỐC TẾ</div>
                             <div className={`${raleway.className} drop-shadow-md`}>RESEARCH ADMINISTRATION AND INTERNATIONAL RELATION OFFICE</div>
                         </div>
-                        <div className="md:ml-[80px] ml-5 flex flex-row gap-5 items-center justify-center">
+                        <div className="md:ml-[80px] flex flex-row gap-5 items-center justify-center">
                             <div class="relative flex items-center w-full h-8 rounded-2xl focus-within:shadow-lg bg-white overflow-hidden">
                                 <div class="grid place-items-center h-full w-12 text-gray-300">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
