539451abdc4b818f2f4ee9d5e8fd6818467bbe7d
vo huu nhan <vohuunhan1310@gmail.com>
2024-04-20 03:50:07 +0700

    fix: header

--- components/component/SubHeader.js
+++ components/component/SubHeader.js
@@ -45,6 +45,37 @@ export default function SubHeader(){
             ]
         }
     ];    
+    const InalCooperation = [
+        {
+            title:"Chương trình hợp tác quốc tế",
+            url:"#"
+        },
+        {
+            title:"Mạng lưới hợp tác",
+            url:"#"
+        },
+        {
+            title:"Dự án quốc tế",
+            url:"#"
+        },
+        {
+            title:"Trao đổi sinh viên",
+            children:[
+                {
+                    name:"Điều kiện",
+                    url:"#"
+                },
+                {
+                    name:"Các trường đối tác",
+                    url:"#"
+                },
+                {
+                    name:"Sinh viên quốc tế",
+                    url:"#"
+                }
+            ]
+        }
+    ]
     return(
         <div className='hidden container max-w-screen-3xl mx-auto md:flex flex-row justify-start items-center px-[75px] gap-5'>
             <a href='/' className='bg-[#164b8e] w-[40px] p-2 flex justify-center items-center'>
@@ -62,8 +93,14 @@ export default function SubHeader(){
                 <li className='relative group'>
                     <DropDownMenu title={"Quản lý khoa học"} children={SManagement}/>
                 </li>
-                <li>Họp tác quốc tế</li>
-                <li>Văn bản - Biểu mẫu</li>
+                <li className='relative group'>
+                    <DropDownMenu title={"Họp tác quốc tế"} children={InalCooperation}/>
+                </li>
+                <li>
+                    <a href='#'>
+                        <div className='py-2 cursor-pointer'>Văn bản - Biểu mẫu</div>
+                    </a>
+                </li>
             </ul>
         </div>
     )
@@ -73,7 +110,7 @@ const DropDownMenu = ({title,children}) =>{
     return(
         <div className='duration-500 transition-all'>
             <div className='cursor-pointer py-2'>{title}</div>
-            <div className='absolute w-[160px] invisible group-hover:visible border-t-4 bg-white drop-shadow-xl border-[#183762]  text-sm leading-loose'>
+            <div className='absolute w-[170px] invisible group-hover:visible border-t-4 bg-white drop-shadow-xl border-[#183762]  text-sm leading-loose'>
                 <ul className='relative z-10'>
                     {children.map((dt,index)=>{
                         if(dt.url){
@@ -93,11 +130,12 @@ const DropDownMenu = ({title,children}) =>{
 }
 
 const DropDownMenuChildren = ({title,children}) =>{
+    console.log(children)
     return(
-        <div className='duration-500 transition-all'>
+        <div>
             <div className='group/item'>
-                <div className='cursor-pointer py-2 hover:bg-[#183762] hover:text-white relative px-1'>{title}</div>
-                <div className='absolute w-[250px] translate-x-[160px] -translate-y-[48px] invisible group-hover/item:visible border-t-4 bg-white drop-shadow-xl border-[#183762]  text-sm leading-loose'>
+                <div className='cursor-pointer py-2 hover:bg-[#183762] hover:text-white px-2'>{title}</div>
+                <div className='absolute w-[250px] translate-x-[170px] -translate-y-[48px] invisible group-hover/item:visible border-t-4 bg-white drop-shadow-xl border-[#183762]  text-sm leading-loose'>
                     <ul className='z-10'>
                         {children.map((dt)=>(
                             <li className='hover:bg-[#183762] hover:text-white px-3 py-2'><a href={dt.url}>{dt.name}</a></li>
